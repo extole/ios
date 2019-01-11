4e36231af191d37dc86445483d3358f5f4e78553
rtibin <rtibin@101.1.168.192.in-addr.arpa>
2019-01-11 16:25:10 +0200

    Initial Commit

---
+++ firstapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  firstapp
+//
+//  Created by rtibin on 1/11/19.
+//  Copyright © 2019 rtibin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

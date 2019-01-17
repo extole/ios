9cb2efb605a96eb8cc4961e839ed2cae716ddaa8
rtibin <rtibin@Mac-mini.localdomain>
2019-01-17 14:05:26 +0200

    adding logger.

---
+++ firstapp/Logger.swift
@@ -0,0 +1,16 @@
+//
+//  Logger.swift
+//  firstapp
+//
+//  Created by rtibin on 1/17/19.
+//  Copyright © 2019 rtibin. All rights reserved.
+//
+
+import Foundation
+
+public class Logger {
+    
+    static func Info(message : String) {
+        print("INFO : \(message)")
+    }
+}
--- firstappTests/firstappTests.swift
+++ firstappTests/firstappTests.swift
@@ -7,19 +7,23 @@
 //
 
 import XCTest
+
 @testable import firstapp
 
 class firstappTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        Logger.Info(message: "setup")
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+         Logger.Info(message: "teardown")
     }
 
     func testExample() {
+        Logger.Info(message: "log example")
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }

201c0b50d90a710ea4cbc2a0d4a661ff9fc8c4b7
rtibin <rtibin@extole.com>
2019-02-25 17:09:45 +0200

    build ExtoleKit.framework, tag with 0.3

---
+++ buildExtoleKit.sh
@@ -0,0 +1,1 @@
+xcodebuild -destination generic/platform=iOS -workspace Workspace.xcworkspace -scheme ExtoleKit CONFIGURATION_BUILD_DIR=Build clean build
--- runTest.sh
+++ runTest.sh
@@ -1,1 +1,9 @@
+set -e
+
+xcodebuild -workspace Workspace.xcworkspace -scheme ExtoleKit CODE_SIGN_IDENTITY= CODE_SIGNING_REQUIRED=NO -destination generic/platform=iOS clean build
+
+xcodebuild -sdk iphonesimulator -workspace Workspace.xcworkspace -scheme ExtoleSanta CODE_SIGN_IDENTITY= CODE_SIGNING_REQUIRED=NO -destination generic/platform=iOS  clean build
+
+xcodebuild -sdk iphonesimulator -workspace Workspace.xcworkspace -scheme SantaPostbox CODE_SIGN_IDENTITY= CODE_SIGNING_REQUIRED=NO -destination generic/platform=iOS  clean build
+
 xcodebuild -sdk iphonesimulator -workspace Workspace.xcworkspace -scheme ExtoleKit CODE_SIGN_IDENTITY= CODE_SIGNING_REQUIRED=NO -destination 'platform=iOS Simulator,name=iPhone SE'  test

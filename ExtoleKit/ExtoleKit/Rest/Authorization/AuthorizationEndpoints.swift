06755073b2a1b0d9edc056a98d3cf13921b2cfdd
rtibin <rtibin@extole.com>
2020-01-21 19:11:26 +0200

    v5TokenUrl in AuthorizationEndpoints

--- ExtoleKit/ExtoleKit/Rest/Authorization/AuthorizationEndpoints.swift
+++ ExtoleKit/ExtoleKit/Rest/Authorization/AuthorizationEndpoints.swift
@@ -3,14 +3,38 @@
 import Foundation
 
 extension ExtoleAPI {
-    public enum Authorization {
-        public static func v5TokenUrl(baseUrl: URL) -> URL {
-          return URL.init(string: "/api/v5/token/", relativeTo: baseUrl)!
+    public enum Authorization {}
+}
+
+
+
+extension ExtoleAPI {
+    var v5TokenUrl: URL {
+        get {
+             return URL.init(string: "/api/v5/token/", relativeTo: baseUrl)!
         }
-        public static func v4TokenUrl(baseUrl: URL) -> URL {
-          return URL.init(string: "/api/v4/token/", relativeTo: baseUrl)!
-        }
-   }
+    }
+    
+    public func createSession(tokenRequest: Authorization.CreateTokenRequest? = nil,
+                               success : @escaping (_: ExtoleAPI.Session) -> Void,
+                               error: @escaping (_: ExtoleAPI.Error) -> Void) {
+         let request = self.network.newJsonRequest(method: "POST", url: v5TokenUrl, headers: [:], data: tokenRequest)
+
+         self.network.processRequest(with: request, success: {token in
+             success(ExtoleAPI.Session(program: self, token: token))
+         }, error: error)
+    }
+
+    public func resumeSession(accessToken: String,
+                              success : @escaping (_: ExtoleAPI.Session) -> Void,
+                              error: @escaping (_: ExtoleAPI.Error) -> Void) {
+         let url = URL.init(string: accessToken, relativeTo: v5TokenUrl)!
+         let empty : String? = nil
+         let request = self.network.newJsonRequest(method: "GET", url: url, headers: [:], data: empty)
+         self.network.processRequest(with: request, success: { token in
+             success(ExtoleAPI.Session(program: self, token: token))
+         }, error: error)
+    }
 }
 
 extension ExtoleAPI.Error {
--- ExtoleKit/ExtoleKit/Rest/ExtoleAPI.swift
+++ ExtoleKit/ExtoleKit/Rest/ExtoleAPI.swift
@@ -5,29 +5,15 @@ import Foundation
 public final class ExtoleAPI {
     let baseUrl: URL
     let network : Network
-    public init(programDomain: String, network: Network = Network.init()) {
+    let appName : String?
+    let appVersion : String?
+    public init(programDomain: String,
+                appName: String? = nil,
+                appVersion: String? = nil,
+                network : Network = Network.init()) {
         self.baseUrl = URL.init(string: "https://" + programDomain)!
+        self.appName = appName
+        self.appVersion = appVersion
         self.network = network
     }
-
-    public func createSession(tokenRequest: Authorization.CreateTokenRequest? = nil,
-                              success : @escaping (_: ExtoleAPI.Session) -> Void,
-                              error: @escaping (_: ExtoleAPI.Error) -> Void) {
-        let request = self.network.newJsonRequest(method: "POST", url: ExtoleAPI.Authorization.v5TokenUrl(baseUrl: baseUrl), headers: [:], data: tokenRequest)
-
-        self.network.processRequest(with: request, success: {token in
-            success(ExtoleAPI.Session(program: self, token: token))
-        }, error: error)
-   }
-
-   public func resumeSession(accessToken: String,
-                             success : @escaping (_: ExtoleAPI.Session) -> Void,
-                             error: @escaping (_: ExtoleAPI.Error) -> Void) {
-        let url = URL.init(string: accessToken, relativeTo: ExtoleAPI.Authorization.v4TokenUrl(baseUrl: baseUrl))!
-        let empty : String? = nil
-        let request = self.network.newJsonRequest(method: "GET", url: url, headers: [:], data: empty)
-        self.network.processRequest(with: request, success: { token in
-            success(ExtoleAPI.Session(program: self, token: token))
-        }, error: error)
-   }
 }
